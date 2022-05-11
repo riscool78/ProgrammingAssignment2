e4e7fd97010e7e7e1b1462ec783e57dd4b3cc1c7
riscool78 <nmcdonald078@gmail.com>
2022-05-11 16:01:24 -0400

    Completed cachematrix assignment with updated script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These functions run sequentially allow for a matrix to be created, its 
+# inverse to be solved and cached in the global environment to save future 
+# computation time. They are intended to work together and provide little 
+# value independently
 
-## Write a short comment describing this function
+
+# This function creates the matrix (or assigns an existing one), 
+# assigns/initializes the  necessary objects, and sets names to the list 
+# objects.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+# Function which checks for cached inverse matrix and returns it if it exists # Otherwise it solves the new inverse matrix and stores it in cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m
 }
